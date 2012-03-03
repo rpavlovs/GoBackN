699eaae2447cbe03392b4b18fbbd27dd3885bab1
Roman Pavlovskyi <rpavlovs@uwaterloo.ca>
2012-03-03 17:29:23 -0500

    Initial Commit

---
+++ src/reciever/reciever.java
@@ -0,0 +1,13 @@
+package reciever;
+
+public class reciever {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/sender/sender.java
@@ -0,0 +1,19 @@
+package sender;
+
+public class sender {
+
+	/**
+	 * @param args
+	 * 
+	 * <host address of the network emulator>
+	 * <UDP port number used by the emulator to receive data from the sender>
+	 * <UDP port number used by the sender to receive ACKs from the emulator>
+	 * <name of the file to be transferred>
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
